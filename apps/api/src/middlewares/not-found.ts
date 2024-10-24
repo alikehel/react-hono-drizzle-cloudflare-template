6fd987672c8fe76e0e3d7610be0d992aa995a0eb
Ali Kehel <ali.saber.27.2@gmail.com>
2024-10-24 03:28:09 +0300

    feat: :fire: improve logging

--- apps/api/src/middlewares/not-found.ts
+++ apps/api/src/middlewares/not-found.ts
@@ -1,11 +1,16 @@
 import { NOT_FOUND } from "@/lib/http-status-codes";
-import { NOT_FOUND as NOT_FOUND_MESSAGE } from "@/lib/http-status-phrases";
+import type { Bindings, Variables } from "@/types/app-bindings";
 import type { NotFoundHandler } from "hono";
 
-export const notFound: NotFoundHandler = (c) => {
+export const notFound: NotFoundHandler<{
+    Bindings: Bindings;
+    Variables: Variables;
+}> = (c) => {
+    c.var.logger.error(`Route not found: ${c.req.url}`);
     return c.json(
         {
-            message: `${NOT_FOUND_MESSAGE} - ${c.req.path}`,
+            success: false,
+            error: `Route not found: ${c.req.url}`,
         },
         NOT_FOUND,
     );
--- apps/api/src/middlewares/on-error.ts
+++ apps/api/src/middlewares/on-error.ts
@@ -1,20 +1,25 @@
 import { INTERNAL_SERVER_ERROR, OK } from "@/lib/http-status-codes";
+import type { Bindings, Variables } from "@/types/app-bindings";
 import type { ErrorHandler } from "hono";
 import type { StatusCode } from "hono/utils/http-status";
-// import { INTERNAL_SERVER_ERROR, OK } from "../lib/http-status-codes.js";
 
-export const onError: ErrorHandler = (err, c) => {
+export const onError: ErrorHandler<{
+    Bindings: Bindings;
+    Variables: Variables;
+}> = (err, c) => {
     const currentStatus =
         "status" in err ? err.status : c.newResponse(null).status;
     const statusCode =
         currentStatus !== OK
             ? (currentStatus as StatusCode)
             : INTERNAL_SERVER_ERROR;
-    const env = c.env?.ENV || c.env?.ENV;
+    // const env = c.env?.ENV || c.env?.ENV;
+    c.var.logger.error(err);
     return c.json(
         {
-            message: err.message,
-            stack: env === "prod" ? undefined : err.stack,
+            success: false,
+            error: err.message,
+            // stack: env === "prod" ? undefined : err.stack,
         },
         statusCode,
     );
