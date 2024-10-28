d21a33945acfa665dd5ee9497dc7f0be18c56cfd
Ali Kehel <ali.saber.27.2@gmail.com>
2024-10-28 11:41:15 +0300

    feat: :fire: add regex for registering

--- apps/api/src/modules/users/schemas/index.ts
+++ apps/api/src/modules/users/schemas/index.ts
@@ -16,7 +16,21 @@ export type User = Pick<
     "id" | "username" | "firstName" | "lastName"
 >;
 
-export const usersInsertSchema = createInsertSchema(usersTable).omit({
+export const usersInsertSchema = createInsertSchema(usersTable, {
+    username: z
+        .string()
+        .regex(
+            /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/gim,
+            "Username can only contain lowercase letters (a-z), numbers (0-9), underscores, and periods. They must not start or end with a period, cannot have multiple periods in a row, and must be no longer than 30 characters",
+        ),
+    password: z
+        .string()
+        .regex(
+            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,
+            "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number. Special characters are allowed",
+        ),
+    //
+}).omit({
     id: true,
 });
 
