4c0c837175dd72bf4363d3d40c0df8cf1e8b0d3d
Ali Kehel <ali.saber.27.2@gmail.com>
2024-10-31 10:03:09 +0300

    feat: :fire: add commands for running migrations on local d1 db

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,20 +1,42 @@
-// import "dotenv/config";
+import fs from "node:fs";
+import path from "node:path";
 import { defineConfig } from "drizzle-kit";
 
+function getLocalD1DB() {
+    try {
+        const basePath = path.resolve(".wrangler");
+        const dbFile = fs
+            .readdirSync(basePath, { encoding: "utf-8", recursive: true })
+            .find((f) => f.endsWith(".sqlite"));
+        if (!dbFile) {
+            throw new Error(`.sqlite file not found in ${basePath}`);
+        }
+        const url = path.resolve(basePath, dbFile);
+        return url;
+    } catch (err) {
+        console.log(`Error  ${err}`);
+    }
+}
+
 export default defineConfig({
+    dialect: "sqlite",
     out: "./src/db/migrations",
     schema: "./src/db/schema/index.ts",
-    dialect: "sqlite",
-    driver: "d1-http",
     verbose: true,
     strict: true,
     casing: "snake_case",
-    dbCredentials: {
-        // biome-ignore lint/style/noNonNullAssertion: <explanation>
-        accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-        // biome-ignore lint/style/noNonNullAssertion: <explanation>
-        databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-        // biome-ignore lint/style/noNonNullAssertion: <explanation>
-        token: process.env.CLOUDFLARE_D1_TOKEN!,
-    },
+    ...(process.env.NODE_ENV === "production"
+        ? {
+              driver: "d1-http",
+              dbCredentials: {
+                  accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
+                  databaseId: process.env.CLOUDFLARE_DATABASE_ID,
+                  token: process.env.CLOUDFLARE_D1_TOKEN,
+              },
+          }
+        : {
+              dbCredentials: {
+                  url: getLocalD1DB(),
+              },
+          }),
 });
